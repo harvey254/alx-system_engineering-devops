cfd22fea5ffffbb84ce169f454868b395e9792e6
harvey254 <harveybill74@gmail.com>
2024-07-10 20:47:55 +0300

    install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 # installs the package puppet-lint
 
+package { 'pip3':
+  ensure => 'present',
+  provider => 'pip3',
+}
+
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['pip3'],
 }
