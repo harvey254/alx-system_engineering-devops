deb566e96c8fef70bcc5ccf579e8c51c71827f14
harvey254 <harveybill74@gmail.com>
2024-07-10 19:22:09 +0300

    config file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,11 @@
 # sets up a client SSH configuration file so that we can connect to a server without using a password
+include stdlib
+file_line { 'Declare identity file':
+#  path    => '/etc/ssh/ssh_config',
+#  line    => '    IdentityFile ~/.ssh/school',
+#  replace => true,
+#}
+
 # client configuration file
 $file_content = file('/etc/ssh/ssh_config')
 $config = "${file_content}\
