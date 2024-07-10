60bdbc6228f45b92fcd0be6e9551dfb98182e195
harvey254 <harveybill74@gmail.com>
2024-07-10 19:28:02 +0300

    config file1

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,19 +1,14 @@
 # sets up a client SSH configuration file so that we can connect to a server without using a password
-include stdlib
+#include stdlib
 file_line { 'Declare identity file':
   path    => '/etc/ssh/ssh_config',
   line    => '    IdentityFile ~/.ssh/school',
   replace => true,
 }
 
-# client configuration file
-$file_content = file('/etc/ssh/ssh_config')
-$config = "${file_content}\
-    IdentityFile ~/.ssh/school
-    PasswordAuthentication no
-"
-file {  'school':
-  ensure  => 'present',
+file_line { 'Turn off passwd auth':
   path    => '/etc/ssh/ssh_config',
-  content => $config
+  line    => '    PasswordAuthentication no',
+  replace => true,
 }
+
