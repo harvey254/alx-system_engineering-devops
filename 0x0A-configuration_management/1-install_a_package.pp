0dbf3046e47b11a0e57824a0072e15ffdd1f22ec
harvey254 <harveybill74@gmail.com>
2024-07-13 19:31:58 +0300

    package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,16 @@
-#!/usr/bin/pip
 #installs puppet-lint
 
+package{ 'python':
+  ensure    => '3.8.10',
+  provider  => 'pip3',
+}
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+package{ 'Werkzeug':
+  ensure   =>'2.1.1',
+  provider =>'pip3',
+  require  => Package['Flask']i
 }
 
