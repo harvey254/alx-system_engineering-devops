a27759896ca0b2c49fd0618a0ad47cebf42f7863
harvey254 <harveybill74@gmail.com>
2024-07-13 14:55:26 +0300

    config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,16 +4,15 @@
 file { 'etc/ssh/ssh_config':
   ensure => 'present',
 }
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  match   => 'PasswordAuthentication yes',
+  replace => 'true',
+}
+
 file_line { 'Declare identity file':
-  ensure  => 'present',
   path    => '/etc/ssh/ssh_config',
   line    => 'IdentityFile ~/.ssh/school',
   match => 'IdentityFile',
 }
-
-file_line { 'Turn off passwd auth':
-  path    => '/etc/ssh/ssh_config',
-  line    => 'PasswordAuthentication no',
-  replace => true,
-}
-
