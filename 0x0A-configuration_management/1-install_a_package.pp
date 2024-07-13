77191855ee7b3639d9b4dfca5068ad5d17e430f3
harvey254 <harveybill74@gmail.com>
2024-07-13 18:48:39 +0300

    bugs

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,7 @@
 #installs puppet-lint
 
-package { 'pip3':
-  ensure => 'present',
-  provider => 'pip3',
-}
-
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['pip3'],
 }
 
